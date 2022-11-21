ebd41b2d6e995ce0b867c3d7ecae49d64c2e87bc
Ripazaha <ripazaha.git@gmail.com>
2022-11-21 12:35:42 +0300

    add Commit

---
+++ src/Worker.java
@@ -0,0 +1,32 @@
+public class Worker {
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+
+    private final OnTaskDoneListener callback;
+    private OnTaskErrorListener callbackError;
+
+    public Worker(OnTaskDoneListener callback) {
+        this.callback = callback;
+    }
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener callbackError) {
+        this.callback = callback;
+        this.callbackError = callbackError;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            callback.onDone("Task " + i + " is done");
+            if (i == 33) {
+                callbackError.onError("Task " + i + " is error");
+            }
+        }
+    }
+}
